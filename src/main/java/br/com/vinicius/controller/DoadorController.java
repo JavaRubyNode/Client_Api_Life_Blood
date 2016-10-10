194f60850478d82cf369abee54ba9388f06dfc6b
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-10-10 16:34:11 -0300

    criação do controller doador

--- src/main/java/br/com/vinicius/controller/DoadorController.java
+++ src/main/java/br/com/vinicius/controller/DoadorController.java
@@ -1,10 +1,32 @@
 package br.com.vinicius.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.vinicius.enums.RedeSocial;
+import br.com.vinicius.enums.TipoSanguineo;
 
 @Controller
 @RequestMapping("/lifeblood")
 public class DoadorController {
+	
+	
+	//-------------------------- Configurando Enum Rede Social na View ----------------------------
+	
+	@ModelAttribute
+	public List<RedeSocial> todasRedeSociais(){return Arrays.asList(RedeSocial.values());}
+	
+	//--------------------------------------------------------------------------------------------
+	
+	//--------------------------Configurando Enum Tipo Sanguineo na View--------------------------
+	
+	@ModelAttribute
+	public List<TipoSanguineo> todosTipoSanguineo(){return Arrays.asList(TipoSanguineo.values());}
+	
+	//---------------------------------------------------------------------------------------------
 
 }

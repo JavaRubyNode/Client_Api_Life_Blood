683b83481c64e3fe4b7adabf74dcd783c19a9e80
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-10-10 17:30:09 -0300

    fazendo alterações no controller do Doador

--- src/main/java/br/com/vinicius/controller/DoadorController.java
+++ src/main/java/br/com/vinicius/controller/DoadorController.java
@@ -6,7 +6,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.vinicius.client.DoadorClient;
+import br.com.vinicius.domain.Doador;
 import br.com.vinicius.enums.RedeSocial;
 import br.com.vinicius.enums.TipoSanguineo;
 
@@ -14,19 +18,43 @@ import br.com.vinicius.enums.TipoSanguineo;
 @RequestMapping("/lifeblood")
 public class DoadorController {
 	
+	private static final String Cadrastro_View ="CadastroDoador";
+	private static final String Lista_View = "ListaDoador";
 	
-	//-------------------------- Configurando Enum Rede Social na View ----------------------------
+	// instanciado objeto client que comunica com a API Web Service
+	
+	private DoadorClient client =new DoadorClient("http://localhost:8080");
+	
+	
+	
+	//-------------Configurando Enum Rede Social na View para ComboBox----------------------------
 	
 	@ModelAttribute
 	public List<RedeSocial> todasRedeSociais(){return Arrays.asList(RedeSocial.values());}
 	
 	//--------------------------------------------------------------------------------------------
 	
-	//--------------------------Configurando Enum Tipo Sanguineo na View--------------------------
+	//-------------Configurando Enum Tipo Sanguineo na View para ComboBox--------------------------
 	
 	@ModelAttribute
 	public List<TipoSanguineo> todosTipoSanguineo(){return Arrays.asList(TipoSanguineo.values());}
 	
 	//---------------------------------------------------------------------------------------------
+	
+	
+	//--------------------------Carregar a tela de Cadastro---------------------------------------------------------------------
+	
+	@RequestMapping("/novo")
+	public ModelAndView novoDoador(){ModelAndView mv = new ModelAndView(Cadrastro_View); mv.addObject(new Doador());return mv;}
+	
+	//-------------------------------------------------------------------------------------------------------------------------
 
+	
+	//----------------------------Metodo Salvar usando HTTP Post-----------------------------------
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(Doador doador){client.salvar(doador);}
+	
+	//---------------------------------------------------------------------------------------------
+	
 }
